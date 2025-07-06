8b4350d6c201e06f9b3497a464dc7524c27bf715
Fred Morrison <frederick.morrison@outlook.com>
2025-07-06 11:05:42 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Python Pandas to Excel Example
+
+An example of using Pandas with a custom function to flatten an arbitrary JSON structure 
+
+## Tools used 
+
+| Tool    |  Version |
+|:--------|---------:|
+| Python  |   3.13.5 |
+| pandas  |    2.3.0 |
+| PyCharm | 2025.1.3 |
+| VSCode  |  1.101.2 |
+
+## Change History
+
+| Date       | Description                  |
+|:-----------|:-----------------------------|
+| 2025-07-01 | Initial creation             |
+
+
+## References
+
+* [Async IO in Python: A Complete Walkthrough](https://realpython.com/async-io-python/)
---
+++ main.py
@@ -0,0 +1,80 @@
+import sys
+from importlib.metadata import version
+
+import pandas as pd
+
+
+def get_python_version() -> str:
+    return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+
+
+def get_package_version(package_name: str) -> str:
+    return version(package_name)
+
+
+def flatten_json(json_info, prefix = ''):
+    """
+    Recursively flattens an arbitrary structured JSON object.
+    Returns a list of flat dicts, each suitable as a row in DataFrame.
+    :param prefix:
+    :type prefix:
+    :type json_info: a JSON structure of unknown schema.
+    """
+    out = []
+
+    if isinstance(json_info, dict):
+        # Dict: flatten each value
+        res = [{}]
+        for k, v in json_info.items():
+            sub = flatten_json(v, prefix + k + '.')
+            new_res = []
+            for item in res:
+                for sub_item in sub:
+                    merged = {**item, **sub_item}
+                    new_res.append(merged)
+            res = new_res
+        out.extend(res)
+
+    elif isinstance(json_info, list):
+        # List: repeat for each element
+        res = []
+        for i, item in enumerate(json_info):
+            sub = flatten_json(item, prefix)
+            res.extend(sub)
+        out.extend(res)
+
+    else:
+        # Leaf value
+        out.append({prefix[:-1]: json_info})
+
+    return out
+
+
+def main():
+    data = {
+        "user": {"name": "Alice", "email": "alice@example.com"},
+        "orders": [
+            {"id": 101, "amount": 99.99},
+            {"id": 102, "amount": 45.50}
+        ]
+    }
+
+    # flattened_data = flatten_user_orders(data)
+    flattened_data = flatten_json(data)
+    df = pd.DataFrame(flattened_data)
+    print(df)
+
+    export_index = True
+    if export_index:
+        df.index.name = 'Sequence'
+
+    # NOTE: by default, to_csv and to_excel overwrite any preexisting file with the same name
+    df.to_csv("customer-orders.csv", index = export_index)
+    df.to_excel("customer-orders.xlsx", index = export_index)
+
+
+if __name__ == '__main__':
+    print(f"Python version: {get_python_version()}")
+    print(f'pandas version: {get_package_version("pandas")}')
+
+    main()
